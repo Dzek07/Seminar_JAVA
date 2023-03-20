c834fed92d264bc3fd95e91948b992a0c9016cd3
Dzek <dzek07@gmail.com>
2023-03-20 21:08:42 +0300

    реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл

--- Lesson_2/2.java
+++ Lesson_2/2.java
@@ -1,33 +1,54 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-
-public class DZ2 {
+public class Main {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        Map<String, String> given = new LinkedHashMap<>();
-        given.put("name","Ivanov");
-        given.put("country","Russia");
-        given.put("city","Moskow");
-        given.put("age",null);
+    
+    int[] anArray = new int[] {3, 2, 7, 0, 4, 9, 2, 6, 8, 3};
 
-        System.out.println(getQuery(given));
+
+    String filePath = "log.txt";
+    try (FileWriter writer = new FileWriter(filePath, true)) {
+        BufferedWriter bufferWriter = new BufferedWriter(writer);
+        bufferWriter.write(Arrays.toString(anArray) + "\n");
+        bufferWriter.close();
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
     }
+    for (int i = 0; i < anArray.length - 1; i++) {
+        for(int j = 0; j < anArray.length - i - 1; j++) {
+            if(anArray[j + 1] < anArray[j]) {
+                int swap = anArray[j];
+                anArray[j] = anArray[j + 1];
+                anArray[j + 1] = swap;
 
-    public static String getQuery(Map<String, String> parameter) {
-        StringBuilder result = new StringBuilder();
-        if (parameter == null || parameter.isEmpty())
-            return result.toString();
+                try (FileWriter writer = new FileWriter(filePath, true)) {
+                    BufferedWriter bufferWriter = new BufferedWriter(writer);
+                    bufferWriter.write(Arrays.toString(anArray) + "\n");
+                    bufferWriter.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
 
-        for (Map.Entry<String, String> pair : parameter.entrySet()) {
-            if (pair.getKey() == null || pair.getValue() == null)
-                continue;
 
-            result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
         }
-
-        if (result.length() > 5)
-            result.delete(result.length() - 5, result.length());
-
-        return result.toString();
+    }
+}
+    System.out.println(Arrays.toString(anArray));   
     }
 }
